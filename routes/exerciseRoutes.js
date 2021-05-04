214ffa8a0b117845353526d586ff634a9810460b
castortroy05 <44813655+castortroy05@users.noreply.github.com>
2021-05-05 00:45:43 +0100

    weeks

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -6,38 +6,61 @@ const controller = require('../controllers/exerciseControllers.js');
 
 router.get("/", controller.landing_page);
 
-router.get('/goals', controller.goals_list);
+const { requiresAuth } = require('express-openid-connect');
 
-router.get('/incompletegoals', controller.incomplete_goals);
+router.get('/profile', requiresAuth(), (req, res) => {
+    res.render('profile', {
+        'picture': req.oidc.user.picture,
+        'nickname': req.oidc.user.nickname,
+        'name': req.oidc.user.name,
+        'email': req.oidc.user.email,
+        'updated': req.oidc.user.updated_at,
+        'user': req.oidc.user.nickname, 
 
-router.get('/completedgoals', controller.completed_goals);
+        
+    });
+  //res.send(JSON.stringify(req.oidc.user));
+});
 
-router.get('/new', controller.new_goal);
+router.get('/goals', requiresAuth(), controller.user_goals_list);
 
-router.post('/new', controller.post_new_goal);
+router.get('/incompletegoals', controller.user_incomplete_goals_list);
+
+router.get('/completedgoals', controller.user_completed_goals_list);
+
+router.get('/new', requiresAuth(), controller.new_goal);
+
+router.post('/new', requiresAuth(), controller.post_new_goal);
 
 router.get('/about', controller.about_page);
 
 router.use('/dbseed', controller.seed);
 
-router.get('delete/:_id', controller.delete_goal);
+router.get('/delete/:id', requiresAuth(), controller.delete_goal);
 
-router.get('view/:_id', controller.view_goal);
+router.get('/view/:id', controller.view_goal);
 
-router.get('edit/:_id', controller.edit_goal);
+router.get('/edit/:id', requiresAuth(), controller.edit_goal);
 
-router.get('share/:_id', controller.share_goal);
+router.get('/completegoal/:id', requiresAuth(), controller.complete_goal);
 
+router.post('/completegoal/:id', requiresAuth(), controller.post_complete_goal);
+
+router.get('/posts/:user', requiresAuth(), controller.user_goals_list);
+
+router.post('/edit/:id', requiresAuth(), controller.post_update_goal);
+
+router.get('/share/:id', requiresAuth(), controller.share_goal);
 
 router.use(function( req, res) {
     res.render('error', {
         'title': 'Error Page',
         'status': 404,
-        'message': 'Page not found',
+        'message': 'Page not found'
         
     });
     res.status(404);
-})
+});
 
 router.use(function(err, req, res, next) {
     
@@ -48,7 +71,7 @@ router.use(function(err, req, res, next) {
         'error': err
     });
     res.status(500);
-    console.log(err)
-})
+    console.log(err);
+});
 
 module.exports = router;
